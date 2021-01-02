a4b965b0d9a6843eccb951c1c7024f9423373931
David Pardines <david.pardinesgim@iesdoctorbalmis.com>
2021-01-02 23:51:47 +0100

    Agregar archivos de proyecto.

---
+++ JuegoAdivinarPelicula/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace JuegoAdivinarPelicula
+{
+    /// <summary>
+    /// Lógica de interacción para MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Pelicula> pelis = new List<Pelicula>();
+        ObservableCollection<string> generos = new ObservableCollection<string>();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            generos.Add("Comedia");
+            generos.Add("Drama");
+            generos.Add("Acción");
+
+
+            Pelicula p1 = new Pelicula("Ejemplo1", "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg", "A", "A", 0);
+            Pelicula p2 = new Pelicula("Ejemplo2", "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg", "A", "A", 0);
+            Pelicula p3 = new Pelicula("Ejemplo3", "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg", "A", "A", 0);
+            Pelicula p4 = new Pelicula("Ejemplo4", "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg", "A", "A", 0);
+            Pelicula p5 = new Pelicula("Ejemplo5", "https://i.blogs.es/5efe2c/cap_001/450_1000.jpg", "A", "A", 0);
+
+            pelis.Add(p1);
+            pelis.Add(p2);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p3);
+            pelis.Add(p4);
+            pelis.Add(p5);
+
+            Pelis.DataContext = pelis;
+            GeneroSeleccionado.ItemsSource = generos;
+        }
+    }
+}
---
+++ JuegoAdivinarPelicula/Pelicula.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace JuegoAdivinarPelicula
+{
+    public class Pelicula : INotifyPropertyChanged
+    {
+        private string _nombre;
+        private string _imagen;
+        private string _pista;
+        private string _genero;
+        private int _dificultad;
+
+        public string Nombre
+        {
+            get => _nombre;
+
+            set
+            {
+                if (this._nombre != value)
+                {
+                    this._nombre = value;
+                    this.NotifyPropertyChanged("Nombre");
+                }
+            }
+        }
+        public string Imagen
+        {
+            get => _imagen;
+
+            set
+            {
+                if (this._imagen != value)
+                {
+                    this._imagen = value;
+                    this.NotifyPropertyChanged("Imagen");
+                }
+            }
+        }
+
+        public string Pista
+        {
+            get => _pista;
+
+            set
+            {
+                if (this._pista != value)
+                {
+                    this._pista = value;
+                    this.NotifyPropertyChanged("Pista");
+                }
+            }
+        }
+
+        public string Genero
+        {
+            get => _genero;
+
+            set
+            {
+                if (this._genero != value)
+                {
+                    this._genero = value;
+                    this.NotifyPropertyChanged("Genero");
+                }
+            }
+        }
+
+        public int Dificultad
+        {
+            get => _dificultad;
+
+            set
+            {
+                if (this._dificultad != value)
+                {
+                    this._dificultad = value;
+                    this.NotifyPropertyChanged("Dificultad");
+                }
+            }
+        }
+
+        public Pelicula(string nombre, string imagen, string pista, string genero, int dificultad)
+        {
+            Nombre = nombre;
+            Imagen = imagen;
+            Pista = pista;
+            Genero = genero;
+            Dificultad = dificultad;
+        }
+
+        public Pelicula()
+        {
+
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            this.PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
